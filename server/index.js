6eecb1013238a6d4ecf9afb7943f452ca3338ad7
siva-mani-r3 <99952847+siva-mani-r3@users.noreply.github.com>
2024-08-14 20:13:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose=require("mongoose")
 const cors=require("cors")
 const app=express()
 app.use(express.json())
+app.use("cors")
 app.use(cors(
     {
         origin:["https://deals-frontend-puce.vercel.app"],
